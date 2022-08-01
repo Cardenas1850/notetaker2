0a81a24cf53516067d4218fb3c9426189e013081
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-08-01 02:29:38 -0400

    set up server js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = requite('express');
+const path = require('path');
+
+const api = require('./routes/notes');
+
+const noteRoutes = require('./routes/noteroute');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/', noteRoutes);
+app.use('/api'), api;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port http:localhost:${PORT}`);
+});
